8750c4154d07d301e85e4d2d556568d7dc5dbfbf
Abhishek/ <abhishekr5821@gmail.com>
2026-05-01 23:29:09 +0530

    final update before deploy

--- ffproject/settings.py
+++ ffproject/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'ffproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ffdb',
+        'USER': 'abhi',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
